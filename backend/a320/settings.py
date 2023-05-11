2009e3fcc74d81145ab50f7004e8ccbd7ae8883b
khadeom <khadeom10@gmail.com>
2023-05-12 02:34:14 +0530

    add manufactory app

--- backend/a320/settings.py
+++ backend/a320/settings.py
@@ -40,10 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
     'authentication',
     'corsheaders',
-
     'rest_framework',
+    #apps
+    'manufactory'
+
+
 ]
 
 MIDDLEWARE = [
@@ -138,3 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
