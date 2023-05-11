966507e17d1ae4f3c3e115e5b98eda9a3ed98b5c
khadeom <khadeom10@gmail.com>
2023-05-12 02:35:19 +0530

    add User Model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('fabrication', 'Fabrication User'),
+        ('sub-assembly', 'Sub-assembly User'),
+        ('assembly', 'Assembly User'),
+    )
+    
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    is_super_user = models.BooleanField(default=False)
