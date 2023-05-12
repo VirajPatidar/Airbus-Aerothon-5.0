b569b1c999cea95701c5c794fdd6b0ed9b53ca34
khadeom <khadeom10@gmail.com>
2023-05-12 07:01:49 +0530

    register models to admin

--- backend/manufactory/admin.py
+++ backend/manufactory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fabrication, SubAssembly, Assembly
 
-# Register your models here.
+admin.site.register(Fabrication)
+admin.site.register(SubAssembly)
+admin.site.register(Assembly)
