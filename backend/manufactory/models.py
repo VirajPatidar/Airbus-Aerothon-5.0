37a79841f969638951ea9431fa8992abb0e99167
khadeom <khadeom10@gmail.com>
2023-05-12 17:51:57 +0530

    fix/typo

--- backend/manufactory/models.py
+++ backend/manufactory/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # TODO we can add plant_name and plane_code to expand it to multiple plants and there own multiple users
 
+
 class Fabrication(models.Model):
     item_id = models.AutoField(primary_key=True)
     item = models.CharField(max_length=255)
@@ -18,14 +19,21 @@ class Machine(models.Model):
     machine_code = models.CharField(max_length=150, unique=True)
     name = models.CharField(max_length=150, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    model_number = models.CharField(max_length=50,null=True, blank=True)
-    machine_type = models.CharField(max_length=50,null=True, blank=True)
+    model_number = models.CharField(max_length=50, null=True, blank=True)
+    machine_type = models.CharField(max_length=50, null=True, blank=True)
 
 
 class SubAssembly(models.Model):
     assembly_id = models.AutoField(primary_key=True)
-    fibrication=models.ForeignKey(Fabrication, verbose_name="Related Fabrication", null=True, on_delete=models.CASCADE)
-    process = models.CharField(max_length=255)   # consider adding this to foregin key and make model
+    fabrication = models.ForeignKey(
+        Fabrication,
+        verbose_name="Related Fabrication",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    process = models.CharField(
+        max_length=255
+    )  # consider adding this to foregin key and make model
     # item_id = models.CharField(max_length=50)   # change this to Foregin key after discussion
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     start_date = models.DateField(auto_now_add=True)
@@ -33,10 +41,12 @@ class SubAssembly(models.Model):
 
     def __str__(self):
         return self.process
+
     def get_item_id(self):
-        if self.fibrication is not None:
-            return self.fibrication.item
+        if self.fabrication is not None:
+            return self.fabrication.item
         return None
+
     def get_machine_id(self):
         if self.machine is not None:
             return self.machine.machine_code
@@ -52,29 +62,32 @@ class SubAssembly(models.Model):
 #     student_id = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='student')
 
 #     def str(self):
-#         return f"{self.class_id} #### {self.student_id}" 
+#         return f"{self.class_id} #### {self.student_id}"
 
 
 class Assembly(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    subassembly = models.ForeignKey(SubAssembly, verbose_name="Reverse SubAssembly",on_delete=models.CASCADE, null=True)   # consider this as Foregin key
+    subassembly = models.ForeignKey(
+        SubAssembly,
+        verbose_name="Reverse SubAssembly",
+        on_delete=models.CASCADE,
+        null=True,
+    )  # consider this as Foregin key
     process = models.CharField(max_length=255)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.process
 
     def get_process_id(self):
         if self.subassembly is not None:
-            return "SAW"+self.subassembly.assembly_id + self.subassembly.get_machine_id()
+            return (
+                "SAW" + self.subassembly.assembly_id + self.subassembly.get_machine_id()
+            )
         return None
+
     def get_machine_id(self):
         if self.machine is not None:
             return self.machine.machine_code
         return None
-
-
-
-
-
