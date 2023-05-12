722c72178074ef29919d19a6d7c476e7e1fe7c0d
Viraj Patidar <vrj381@gmail.com>
2023-05-12 23:57:44 +0530

    routing + global state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import Navbar from './components/navbar';
 import NotFound from './components/notFound';
 import Home from './components/home';
 import Test from './components/test';
+import Unauthorized from './components/unauthorized';
+import Login from './components/auth/login';
+import ProtectedRoutes from './components/auth/ProtectedRoutes';
+import FabricationDashboard from './components/dashboards/fabrication/fabricationDashboard';
+import SubAssemblyDashboard from './components/dashboards/subAssembly/subAssembly';
+import AssemblyDashboard from './components/dashboards/assembly/assemblyDashboard';
 
 // MUI
 import { blue } from '@mui/material/colors';
@@ -25,6 +31,21 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} exact />
+                <Route path="/login" element={<Login />} exact />
+                <Route path="/unauthorized" element={<Unauthorized />} exact />
+
+                <Route element={<ProtectedRoutes allowedRoles='fabrication' />}>
+                    <Route path="/fabrication-dashboard" element={<FabricationDashboard />} />
+                </Route>
+
+                <Route element={<ProtectedRoutes allowedRoles='sub-assembly' />}>
+                    <Route path="/sub-assembly-dashboard" element={<SubAssemblyDashboard />} />
+                </Route>
+
+                <Route element={<ProtectedRoutes allowedRoles='assembly' />}>
+                    <Route path="/assembly-dashboard" element={<AssemblyDashboard />} />
+                </Route>
+
                 <Route path="/test" element={<Test />} exact />
                 <Route path="*" element={<NotFound />} />
             </Routes>
